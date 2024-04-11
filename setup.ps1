48da026fe6c04ab06ca4baae3279f2a0c9b6446e
Roken47 <mradeboer7@gmail.com>
2024-04-11 14:23:35 -0500

    setup.ps1 init

---
+++ setup.ps1
@@ -0,0 +1,142 @@
+# Scoop Installed 
+# APP              BUCKET
+# 7zip              main
+# bat                main
+# bottom             main
+# chezmoi            main
+# discord            extras
+# eza                main
+# file               main
+# firefox-developer  versions
+# git-with-openssh   main
+# grep               main
+# helix              main
+# neofetch           main   
+# pasteboard         extras
+# scoop-search       main
+# starship           main
+# sudo               main
+# touch              main
+# transmission       extras
+# uutils-coreutils   main
+# vcredist2022       extras
+# vim                main
+# which              main
+# winmtr             extras
+# zoxide             main
+# zstd               main
+
+# Choco Installed
+# alacritty 
+# chocolateygui
+# flameshot 
+# gawk 
+# nerd-fonts-AnonymousPro
+#ui
+
+# Installed
+# Name                              Id                                 
+# ---------------------------------------------------------------------
+# Wintoys                           11413PtruceanBogdan.Wintoys_ankwh… 
+# Bitwarden                         #.#                  
+# Simplenote                        2# 24# 90Automattic.Simplenote_9h07f7… 
+# Anytype 0.39.0                    Any# Asso# ciation.Anytype             
+# Diarium                           49297T# .Partl.# DailyDiary_jr9bq2af9… 
+# Signal 7.4.0                      OpenWhis# perSystem# s.Signal          
+# HP Smart                          AD2F1837.H# PPrinterCon# trol_v10z8vj… 
+# iCloud                            AppleInc.iCl# oud_nzyj5cx40# ttqa      
+# Battle.net                        Battle.net    #                #       
+# Bitdefender Antivirus Plus        Bitdefender     #                  #   
+# Bitdefender Agent                 Bitdefender.Bitde# fender           #  
+# Bitdefender VPN                   Bitdefender VPN     #              #   
+# Ubuntu                            Canonical.Ubuntu.2204 #            #   
+# Ubuntu on Windows                 CanonicalGroupLimited.Ub# untuonWin# … 
+# Diablo IV                         Diablo IV                 #        #   
+# Everything 1.4.1.1024 (x64)       voidtools.Everything        #      #   
+# Flow Launcher                     Flow-Launcher.Flow-Launcher   #    #   
+# f.lux                             flux.flux                       #  #   
+# FreeCAD 0.21.2 (Installed for Cu… FreeCAD.FreeCAD                  #   
+# Git                               Git.Git                          #   
+# GNU Privacy Guard                 GnuPG.GnuPG                      #   
+# Gpg4win (4.3.1)                   GnuPG.Gpg4win                    #   
+# HandBrake 1.7.3                   HandBrake.HandBrake              #   
+# Logitech Gaming Software 9.04     Logitech.LGS                     #   
+# MakeMKV v1.17.6                   GuinpinSoft.MakeMKV               #  
+# Mesh Agent                        Mesh Agent                       #   
+# Minecraft Launcher                Microsoft.4297127D64EC6_8wekyb3d8# … 
+# App Installer                     Microsoft.AppInstaller           #   
+# HEIF Image Extensions             Microsoft.HEIFImageExtension_8wek# … 
+# HEVC Video Extensions from Devic… Microsoft.HEVCVideoExtension_8wek# … 
+# Minecraft for Windows             Microsoft.MinecraftUWP_8wekyb3d8b# … 
+# Windows Terminal                  Microsoft.WindowsTerminal        #   
+# Windows Package Manager Source (… Microsoft.Winget.Source_8wekyb3d8# … 
+# Windows Subsystem for Linux       MicrosoftCorporationII.WindowsSub# … 
+# NVIDIA Control Panel              NVIDIACorp.NVIDIAControlPanel_56j# … 
+# Notepad++ (32-bit x86)            Notepad++.Notepad++              #   
+# Playnite                          Playnite.Playnite                #   
+# Microsoft Office Professional Pl… ProPlus2021Retail - en-us        #   
+# Rainmeter                         Rainmeter.Rainmeter                #                                   
+# Pocket Casts Desktop              ShiftyJelly.PocketCastsDesktop_jr# … 
+# StarCraft                         StarCraft                        #   
+# Steam                             Valve.Steam                     #   
+# Cyberpunk 2077                    Steam App 1091500               #   
+# RuneScape                         Steam App 1343400                 #  
+# Downfall - A Slay the Spire Fan … Steam App 1865780                #   
+# The Outer Worlds: Spacer's Choic… Steam App 1920490                #   
+# Wolfenstein 3D                    Steam App 2270                   #   
+# Warframe                          DigitalExtremes.Warfram# e 
+# Stardew Valley                    Steam App 413150         #           
+# Hellblade: Senua's Sacrifice      Steam App 414340           #         
+# The Outer Worlds                  Steam App 578650             #       
+# Slay the Spire                    Steam App 646570               #     
+# Counter-Strike: Global Offensive  Steam App 730                   #   
+# TeamViewer                        TeamViewer.TeamViewer            #   
+# Debian                            Debian.Debian                   #   
+# Uninstall Tool                    CrystalIDEASoftware.UninstallToo# l 
+# Ubisoft Connect                   Ubisoft.Connect                  #   
+# VLC media player                  VideoLAN.VLC                    #   
+# Vivaldi                           Vivaldi                           #  
+# balenaEtcher 1.18.11              Balena.Etcher                     #  
+# Beeper 3.102.16                   d8e15c19-204f-5d84-8a21-1b24e3845…#  
+# iMazing                           DigiDNA.iMazing                  #  
+# WinSCP 6.3.2                      WinSCP.WinSCP                     #  
+# MPC-HC 2.1.7.2 (d5cb0a822) Night… clsid2.mpc-hc                    #   
+# Python Launcher                   Python.Launcher                  #   
+# Private Internet Access           PrivateInternetAccess.PrivateInte# … 
+# File Juggler 3.0.5                {35FF4CFD-0C31-4C75-AF46-38E432B6# … 
+# Jellyfin Media Player             Jellyfin.JellyfinMediaPlayer       #                                   
+# MSI Live Update 6                 {4F46CF54-47D2-41F4-B230-B0954C54…   #                      
+# Bonjour                           {56DDDFB8-7F79-4480-89D5-25E1F52A…     #                     
+# MSYS2                             {5fa5cbe1-3929-48ba-ab8a-39e37b13# … 
+# Nextcloud                         Nextcloud.NextcloudDesktop         #                                   
+# Mailbird                          Mailbird.Mailbird                    #                                                               
+# Microsoft Visual C++ 2005 Redist… Microsoft.VCRedist.2005.x86            #                            
+# REDlauncher                       {7258BA11-600C-430E-A759-27E2C691# … 
+# Teams Machine-Wide Installer      {731F6BAA-A986-45A4-8936-7C3AAAAA# … 
+# Apple Mobile Device Support       {74CC99EB-7DC0-4CB0-847A-F8C2FE3# 9…
+# Microsoft Visual Studio Code (Us… Microsoft.VisualStudioCode        #                                  
+# Intel Extreme Tuning Utility      {7afa48c7-9901-40fa-8f9b-f0707e2b# … 
+# MSIRegister                       {80B995A4-3A86-4690-98A6-563F1A78# … 
+# MSI Command Center                {85A2564E-9ED9-448A-91E4-B9211EE5# … 
+# Realtek Ethernet Controller Driv… {8833FFB6-5B0C-4764-81AA-06DFEED9# … 
+# Oracle VM VirtualBox 7.0.14       Oracle.VirtualBox                  #                              
+# Python Launcher                   Python.Launcher      #   
+# MSI MysticLight                   {93874B70-6C5E-446A-AF4# D-E5AC776A… 
+# MTG Arena                         {9BB36466-75B7-4217-A7C9-# CD6462E6… 
+# Corsair iCUE5 Software            Corsair.iCUE.5             #         
+# PowerShell 7-x64                  Microsoft.PowerShell         #      
+# NVIDIA Graphics Driver 552.12     {B2FE1952-0186-46C3-BAEC-A80AA3# 5A… 
+# NVIDIA PhysX System Software 9.2… Nvidia.PhysX                     #                            
+# NVIDIA FrameView SDK 1.3.8513.32… {B2FE1952-0186-46C3-BAEC-A80AA35A# … 
+# NVIDIA HD Audio Driver 1.3.40.14  {B2FE1952-0186-46C3-BAEC-A80AA35A# … 
+# NVCleanstall                      {B422A5B9-1671-4E8B-BD8B-1E76A2AB# … 
+# Rio                               {B44D113C-43CF-40FA-8F4B-15441ECA# … 
+# Microsoft GameInput               {B71DA3AB-24EC-9E95-A79B-7B5F92B0# … 
+# Cider                             {CF1487D2-EF49-46D8-98AF-0309E35E# … 
+# nu                                Nushell.Nushell                    #                                    
+# iPod Support                      {DEC0F5DF-216B-4D66-B3DD-B1BDDC7A# … 
+# Flameshot                         Flameshot.Flameshot                #                                    
+# iCloud Outlook                    {F054257C-600A-4918-B730-F6829E49…   #                           
+# Realtek High Definition Audio Dr… {F132AF7F-7BCA-4EDE-8A7C-958108FE# … 
+# Windows Subsystem for Linux Upda… {F8474A47-8B5D-4466-ACE3-78EAB3BF… #                               
+# Python 3.10.11 (64-bit)           Python.Python.3.10               #   
